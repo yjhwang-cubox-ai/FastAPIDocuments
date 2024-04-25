826d799783cb91c73eed9ca57d086a480495d04d
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2024-04-25 10:56:19 +0900

    feat: json schema

--- body.py
+++ body.py
@@ -1,23 +1,54 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Path, Query, Body
+from fastapi import Body, FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+
 
 class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
     tax: float | None = None
-    
-@app.put('/items/{item_id}')
+
+
+@app.put("/items/{item_id}")
 async def update_item(
+    *,
     item_id: int,
-    item: Annotated[Item, Body(embed=True)],
-    q: str | None = None
+    item: Annotated[
+        Item,
+        Body(
+            openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
+        ),
+    ],
 ):
     results = {"item_id": item_id, "item": item}
-    if q:
-        results.update({"q": q})
     return results
